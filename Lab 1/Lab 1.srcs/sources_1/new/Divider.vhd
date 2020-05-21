81d9238bdfb8d9ba969f3e6abead9f85c2efe97e
SulhiCader1 <sulhicader1@gmail.com>
2020-05-21 17:01:23 +0530

    Added Initial component files

---
+++ Lab 1/Lab 1.srcs/sources_1/new/Divider.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/21/2020 04:57:47 PM
+-- Design Name: 
+-- Module Name: Divider - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Divider is
+--  Port ( );
+end Divider;
+
+architecture Behavioral of Divider is
+
+begin
+
+
+end Behavioral;

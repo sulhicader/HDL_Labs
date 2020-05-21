5ce906da8d9f8aa50d588616b2121765d7447bb7
SulhiCader1 <sulhicader1@gmail.com>
2020-05-21 16:33:38 +0530

    Added initial Synch

--- Lab 1/Lab 1.sim/sim_1/behav/xsim/xsim.dir/FSM_tb_behav/webtalk/xsim_webtalk.tcl
+++ Lab 1/Lab 1.sim/sim_1/behav/xsim/xsim.dir/FSM_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
-webtalk_init -webtalk_dir C:/Users/M.F.M.MUAZ/Desktop/HDL lab/HDL_Labs/Lab 1/Lab 1.sim/sim_1/behav/xsim/xsim.dir/FSM_tb_behav/webtalk/
+webtalk_init -webtalk_dir C:/Users/Dell/Desktop/HD/HDL_Labs/Lab 1/Lab 1.sim/sim_1/behav/xsim/xsim.dir/FSM_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Thu May 21 14:58:20 2020" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Thu May 21 16:09:30 2020" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2019.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2708876" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -12,9 +12,9 @@ webtalk_add_data -client project -key target_family -value "not_applicable" -con
 webtalk_add_data -client project -key target_device -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_package -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
-webtalk_add_data -client project -key random_id -value "08aa0423-9690-4a76-b821-52cd2d76ae0c" -context "software_version_and_target_device"
+webtalk_add_data -client project -key random_id -value "690d7239-0467-45d3-bed8-7a64feb883a3" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "50d81890ec5c4357a3302d227fb4159e" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "23" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "24" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-7200U CPU @ 2.50GHz" -context "user_environment"
@@ -36,7 +36,7 @@ webtalk_add_data -client xsim -key Total_Nets -value "0" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Processes -value "4" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Instances -value "5" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Xilinx_HDL_Libraries_Used -value "secureip " -context "xsim\\usage"
-webtalk_add_data -client xsim -key Compiler_Time -value "1.01_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Compiler_Memory -value "44300_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1745000001 -regid "" -xml C:/Users/M.F.M.MUAZ/Desktop/HDL lab/HDL_Labs/Lab 1/Lab 1.sim/sim_1/behav/xsim/xsim.dir/FSM_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/M.F.M.MUAZ/Desktop/HDL lab/HDL_Labs/Lab 1/Lab 1.sim/sim_1/behav/xsim/xsim.dir/FSM_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/M.F.M.MUAZ/Desktop/HDL lab/HDL_Labs/Lab 1/Lab 1.sim/sim_1/behav/xsim/xsim.dir/FSM_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Compiler_Time -value "0.95_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Compiler_Memory -value "43712_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2830254197 -regid "211950680_0_0_701" -xml C:/Users/Dell/Desktop/HD/HDL_Labs/Lab 1/Lab 1.sim/sim_1/behav/xsim/xsim.dir/FSM_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Dell/Desktop/HD/HDL_Labs/Lab 1/Lab 1.sim/sim_1/behav/xsim/xsim.dir/FSM_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Dell/Desktop/HD/HDL_Labs/Lab 1/Lab 1.sim/sim_1/behav/xsim/xsim.dir/FSM_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
